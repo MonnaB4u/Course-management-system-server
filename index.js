b3e2f5a364f0ff7fad5788ae5ff9097ecbd06c81
Moheuddin Monna <moheuddin.monnab4u@gmail.com>
2021-05-26 16:54:14 +0800

    done

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
 
-const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.dyvn0.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_pass}@cluster0.dyvn0.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
 
 const app = express()
-
 app.use(bodyParser.json());
 app.use(cors());
+const port = 5000;
 
-const port = 5000
-// console.log(process.env.DB_User + "  ___ "+process.env.db_pass )
+
+
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -21,35 +21,110 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
 
   const allCourses = client.db("CourseManagement").collection("allCourses");
-  console.log("Db Connected")
- 
+    const gradeBook = client.db("CourseManagement").collection("gradeBook");
+    const UploadSyllabus = client.db("CourseManagement").collection("uploadSyllabus");
+    const Notification = client.db("CourseManagement").collection("Notification");
+    const Assignment = client.db("CourseManagement").collection("Assignment");
 
-//for send data in data base from the fakedata
+  console.log("port is" + {port} + "Db Connected")
 
+ //for send data in data base from the fakedata
+  
  app.post('/addCourse',(req,res)=>{
-     const course=req.body;
-     allCourses.insertMany(course)
-     .then(result =>{
-         console.log(result.insertedCount);
-         res.send(result.insertedCount)
-     })
- })
-
-  app.get('/courseall',(req,res)=>{
-    allCourses.find({}).limit(6)
-    .toArray((err,documents)=>{
-        res.send(documents)
-    })
+  const course=req.body;
+  allCourses.insertMany(course)
+  .then(result =>{
+      console.log(result.insertedCount);
+      res.send(result.insertedCount)
   })
+})
+
+app.get('/courseall',(req,res)=>{
+ allCourses.find({})
+ .toArray((err,documents)=>{
+     res.send(documents)
+ })
+})
 
 
-////next step
+////uploadSyllabus
+  
+app.post('/addSyllabus',(req,res)=>{
+  const syllabus=req.body;
+  console.log("/addSyllabus")
+  UploadSyllabus.insertOne(syllabus)
+  .then(result =>{
+      console.log(result.insertedCount);
+      res.send(result.insertedCount)
+  })
+})
+
+app.get('/syllabusall',(req,res)=>{
+  UploadSyllabus.find({})
+ .toArray((err,documents)=>{
+     res.send(documents)
+ })
+})
+   
+////Grade
+  
+  
+app.post('/addGrades',(req,res)=>{
+  const grade=req.body;
+  console.log("/addSyllabus")
+  gradeBook.insertOne(grade)
+  .then(result =>{
+      console.log(result.insertedCount);
+      res.send(result.insertedCount)
+  })
+})
+
+app.get('/Gradeeall',(req,res)=>{
+  gradeBook.find({})
+ .toArray((err,documents)=>{
+     res.send(documents)
+ })
+})
+
+ ////Notification
+
+ app.post('/addNotification',(req,res)=>{
+  const feed=req.body;
+  Notification.insertOne(feed)
+  .then(result =>{
+      console.log(result.insertedCount);
+      res.send(result.insertedCount)
+  })
+})
+
+app.get('/allNotification',(req,res)=>{
+  Notification.find({})
+ .toArray((err,documents)=>{
+     res.send(documents)
+ })
+})
+
+///Assignments
+
+app.post('/addAssignments',(req,res)=>{
+  const feedasas=req.body;
+  Assignment.insertOne(feedasas)
+  .then(result =>{
+      console.log(result.insertedCount);
+      res.send(result.insertedCount)
+  })
+})
+
+app.get('/allAssignments',(req,res)=>{
+  Assignment.find({})
+ .toArray((err,documents)=>{
+     res.send(documents)
+ })
+})
+
+
 
 });
 
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-app.listen(port);
+app.listen(port)
